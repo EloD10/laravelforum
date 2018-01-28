f6917acb0d8d14b700d0d5fd3855cb809c205263
EloD10 <elo210@outlook.fr>
2018-01-28 20:18:23 +0100

    fix show view and margin menu

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,16 +2,13 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{ $thread->title  }}</div>
-
-                    <div class="panel-body">
-                     {{ $thread->body }}
-                    </div>
+        <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">
+                            {{$thread->title }}
+                    </h5>
+                    <p class="card-text">{{ $thread->body }}</p>
                 </div>
-            </div>
         </div>
     </div>
 @endsection
