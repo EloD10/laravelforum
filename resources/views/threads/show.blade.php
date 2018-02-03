0bccc74f8ad8e4816b2114bff82f036e58a8e4a5
EloD10 <elo210@outlook.fr>
2018-02-03 17:32:55 +0100

    Add replies

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -7,8 +7,30 @@
                     <h5 class="card-title">
                             {{$thread->title }}
                     </h5>
-                    <p class="card-text">{{ $thread->body }}</p>
+                    <p class="card-text">
+                        {{ $thread->body }}
+                    </p>
                 </div>
         </div>
     </div>
+
+    @foreach ($thread->replies as $reply)
+    <div class="container">
+        <div class="card" style="margin: 1em;">
+            <h5 class="card-header">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a>
+                said {{ $reply->created_at->diffForHumans() }}...
+            </h5>
+
+            <div class="card-body">
+                <p class="card-text">
+                    {{ $reply->body }}
+                </p>
+            </div>
+        </div>
+    </div>
+    @endforeach
+
 @endsection
