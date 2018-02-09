cb7c395488595c03350f9bca6ce9869ce8519f2f
EloD10 <elo210@outlook.fr>
2018-02-09 21:51:06 +0100

    Fix Bootstrap details

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+
     <!-- Thread -->
-    <div class="container">
-        <div class="card">
+    <div class="card card-default">
+           <div class="card-header">
+                <h5>
+                    <a href="#"> {{ $thread->creator->name }}</a> posted :
+                    <strong>{{$thread->title }}</strong>
+                </h5>
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title">
-                        <a href="#"> {{ $thread->creator->name }}</a> posted :
-                        <strong>{{$thread->title }}</strong>
-                    </h5>
-                    <p class="card-text">
+                    <p>
                         {{ $thread->body }}
                     </p>
                 </div>
         </div>
-    </div>
 
     <!-- Replies -->
-    <div class="container">
         @foreach ($replies as $reply)
             @include('threads.reply')
         @endforeach
@@ -28,19 +29,23 @@
 
      <!-- Write a reply -->
         @if (auth()->check())
-            <form method="POST" action="{{ $thread->path() . '/replies' }}">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <textarea name="body" class="form-control" rows="3" placeholder="Submit a reply"></textarea>
-                </div>
+        <div class="card card-default">
+            <div class="card-header">Write a response</div>
+            
+            <div class="card-body">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" class="form-control" rows="3" placeholder="Submit a reply"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                @else
+                    <p>Please <a href="{{ route('login') }}">sign in</a> for submit replies</p>
+                @endif
+            </div>
+        </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-        @else
-            <p>Please <a href="{{ route('login') }}">sign in</a> for submit replies</p>
-        @endif
-
-    </div>
-
+</div>
 
 @endsection
